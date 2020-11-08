9dfb537871752f5f2011ced31ddd0722707ffc91
FooQoo <horseshoes.buzz.17@gmail.com>
2020-11-08 12:52:15 +0900

    【実装】ポスト追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `FooQoo's memo`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `FooQoo`,
+      summary: `Web Enginner`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `日常生活の気付きをメモします`,
+    siteUrl: `https://fooqoo56.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `FooQoo17`,
     },
   },
   plugins: [
